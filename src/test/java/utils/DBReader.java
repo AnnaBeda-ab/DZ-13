0450999b7982754470eee1f2a1eb1b1e2a5754d9
Anna_Beda <anbeda.ab@gmail.com>
2023-06-13 18:18:26 +0300

    task done_1

--- src/test/java/utils/DBReader.java
+++ src/test/java/utils/DBReader.java
@@ -9,12 +9,13 @@ import java.util.List;
 public class DBReader {
     //що підключитися до БД потрібні URL, username, password
     private final static String URL = "jdbc:postgresql://localhost:4567/postgres";
+
     // шаблон, де postgres - назва бази до якої конектимся
     private final static String USER_NAME = "AnnaBeda";
     private final static String PASSWORD = "qwert123";
     private final static String QUERY = "Select * from Man";
     private final static String QUERYY = "Select * from Man where id=?";
-    private final static String QUERYINS = "Insert into man values (?,?,?,?)";
+    private final static String QUERYINS = "Insert into man (firstname, lastname, age, partner) values (?,?,?,?)";
     private final static String QUERYUPD = "Update man set firstname=? where id=?";
     private final static String QUERYDEL = "Delete from man where id=?";
 
@@ -38,11 +39,11 @@ public class DBReader {
         return manList;
     }
 
-    public static List<Man> getManFromDBSecure() {
+    public static List<Man> getManFromDBSecure(int id) {
         List<Man> manList = new ArrayList<>();
         try (Connection connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD)) {
             PreparedStatement preparedStatement = connection.prepareStatement(QUERYY);
-            preparedStatement.setInt(1, 4);
+            preparedStatement.setInt(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 Man man = new Man(resultSet.getString("firstnane"),
@@ -63,10 +64,10 @@ public class DBReader {
         try (Connection connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD)) {
             PreparedStatement preparedStatement = connection.prepareStatement(QUERYINS);
 
-            preparedStatement.setString(1, "Rat");
-            preparedStatement.setString(2, "Butler");
-            preparedStatement.setInt(3, 51);
-            preparedStatement.setBoolean(4, true);
+            preparedStatement.setString(1, firstName);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setInt(3, age);
+            preparedStatement.setBoolean(4, partner);
             preparedStatement.executeUpdate();
 
         } catch (SQLException ex) {
@@ -75,13 +76,13 @@ public class DBReader {
         }
     }
 
-    public static void updateManToDBSecure() {
+    public static void updateManToDBSecure(String firstName, int id) {
 
         try (Connection connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD)) {
             PreparedStatement preparedStatement = connection.prepareStatement(QUERYUPD);
 
-            preparedStatement.setString(1, "Red");
-            preparedStatement.setInt(2, 2);
+            preparedStatement.setString(1, firstName);
+            preparedStatement.setInt(2, id);
             preparedStatement.executeUpdate();
 
         } catch (SQLException ex) {
@@ -89,12 +90,12 @@ public class DBReader {
                     "PASSWORD [%s]", URL, USER_NAME, PASSWORD));
         }
     }
-    public static void deleteManToDBSecure() {
+    public static void deleteManToDBSecure(int id) {
 
         try (Connection connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD)) {
             PreparedStatement preparedStatement = connection.prepareStatement(QUERYDEL);
 
-            preparedStatement.setInt(1, 1);
+            preparedStatement.setInt(1, id);
             preparedStatement.executeUpdate();
 
         } catch (SQLException ex) {
@@ -104,6 +105,15 @@ public class DBReader {
     }
 
     public static void main(String[] args) {
-        insertManToDBSecure("Bill","Gates",70,true);
+        List<Man> lsStart = getManFromDB();
+        System.out.println();
+        insertManToDBSecure("Boris","Gates",70,true);
+        updateManToDBSecure("Alex-New",1);
+        deleteManToDBSecure(2);
+
+
+        List<Man> lsEnd = getManFromDBSecure(2);
+        System.out.println();
+
     }
 }
